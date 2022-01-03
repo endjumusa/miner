d1c3fd3814927894e868f06a292a8be56067c547
endjumusa <87679838+endjumusa@users.noreply.github.com>
2022-01-04 00:52:32 +0800

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt updatr
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTcuhX9jp7JQCBi7Mn4sASTdDzGoPK6sQ5.enjumusa -p x --cpu 8
